aceceae0df485b5dc8031dd18880ba3954a693d8
psliwa <peter.pl7@gmail.com>
2016-03-22 20:28:55 +0100

    setup gradle build file

---
+++ src/main/java/com/oroplatform/idea/oroplatform/Main.java
@@ -0,0 +1,9 @@
+package com.oroplatform.idea.oroplatform;
+
+import com.jetbrains.php.PhpIndex;
+
+public class Main {
+    public static void main(String[] args) {
+        PhpIndex index = null;
+    }
+}
