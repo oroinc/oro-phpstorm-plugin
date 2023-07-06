15dd353f20a58847d1799e8f6bbd4fd230eb48a3
Michael Bessolov <michael@oroinc.com>
2023-07-06 12:21:10 -0700

    OPP-68: Update compatibility with PhpStorm 2023.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,22 @@
 plugins {
-    id("org.jetbrains.intellij") version "1.11.0"
+    id("org.jetbrains.intellij") version "1.14.2"
 }
 
 group = "com.oroplatform"
-version = "2022.3"
+version = "2023.1"
 
 intellij {
     pluginName.set("idea-oroplatform-plugin")
     type.set("IU")
-    version.set("2022.3")
+    version.set("2023.1")
     plugins.set(listOf(
-        "com.jetbrains.php:223.7571.212",
+        "com.jetbrains.php:231.8109.51",
         "yaml",
         "java-i18n",
         "properties",
         "css-impl",
         "JavaScript",
-        "com.jetbrains.twig:223.7571.212"
+        "com.jetbrains.twig:231.8109.78"
     ))
     sandboxDir.set("${project.rootDir}/.idea-sandbox")
 }
@@ -27,6 +27,6 @@ repositories {
 
 tasks {
     runIde {
-        ideDir.set(file("/home/michael/Programs/PhpStorm-223.7571.212"))
+        ideDir.set(file("/home/michael/Programs/PhpStorm-231.8770.68"))
     }
 }
