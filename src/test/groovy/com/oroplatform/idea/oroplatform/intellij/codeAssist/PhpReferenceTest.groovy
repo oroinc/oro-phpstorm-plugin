a7a0d8599930f44733fa0d7c66318400711bc5e2
Aleksander Mosingiewicz <amosingiewicz@oroinc.com>
2023-10-30 15:47:29 +0100

    OPP-80: Fixes in PHP reference test and Workflow Completion Test.

--- src/test/groovy/com/oroplatform/idea/oroplatform/intellij/codeAssist/PhpReferenceTest.groovy
+++ src/test/groovy/com/oroplatform/idea/oroplatform/intellij/codeAssist/PhpReferenceTest.groovy
@@ -1,25 +1,11 @@
 package groovy.com.oroplatform.idea.oroplatform.intellij.codeAssist
 
-import groovy.com.oroplatform.idea.oroplatform.intellij.codeAssist.CompletionTest;
 import com.intellij.psi.PsiPolyVariantReferenceBase
-import com.intellij.testFramework.LoggedErrorProcessor
 import com.jetbrains.php.lang.psi.elements.PhpNamedElement
-import org.apache.log4j.Logger
-import org.jetbrains.annotations.NotNull
+import com.intellij.openapi.project.ProjectUtil;
 
 
 abstract class PhpReferenceTest extends CompletionTest {
-
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp()
-
-        //turn off falling tests on internal errors because there is bug in php plugin during indexing class with field
-//        LoggedErrorProcessor.setNewInstance(new LoggedErrorProcessor() {
-//            void processError(String message, Throwable t, String[] details, @NotNull Logger logger) {
-//            }
-//        })
-    }
 
     def checkPhpReference(String content, List<String> expectedReferences) {
         assertEquals(expectedReferences, getPhpReference(content))
@@ -28,7 +14,7 @@ abstract class PhpReferenceTest extends CompletionTest {
     def List<String> getPhpReference(String content) {
         configureByText(content)
 
-        myFixture.getProject().getBaseDir().refresh(false, true)
+        ProjectUtil.guessProjectDir(myFixture.getProject()).refresh(false, true)
 
         def element = myFixture.getFile().findElementAt(myFixture.getCaretOffset())
         def elements = [element, element.getParent(), element.getParent().getParent()]
@@ -42,11 +28,5 @@ abstract class PhpReferenceTest extends CompletionTest {
             .collect { (it.getElement() as PhpNamedElement).getFQN().stripMargin('\\') }
             .unique()
             .toList()
-    }
-
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown()
-        LoggedErrorProcessor.setNewInstance(new LoggedErrorProcessor())
     }
 }
