f034a0a56e0c123e4030b31b2c598f6a79b9f302
Aleksander Mosingiewicz <amosingiewicz@oroinc.com>
2023-12-18 14:15:00 +0100

    OPP-75: Replace ScalarIndexExtension.VOID_DATA_EXTERNALIZER with suggested equivalent.

--- src/main/java/com/oroplatform/idea/oroplatform/intellij/indexes/ImportFileBasedIndex.java
+++ src/main/java/com/oroplatform/idea/oroplatform/intellij/indexes/ImportFileBasedIndex.java
@@ -5,6 +5,7 @@ import com.intellij.util.indexing.*;
 import com.intellij.util.io.DataExternalizer;
 import com.intellij.util.io.EnumeratorStringDescriptor;
 import com.intellij.util.io.KeyDescriptor;
+import com.intellij.util.io.VoidDataExternalizer;
 import com.oroplatform.idea.oroplatform.intellij.codeAssist.PsiElements;
 import com.oroplatform.idea.oroplatform.intellij.codeAssist.yml.YamlPsiElements;
 import com.oroplatform.idea.oroplatform.settings.OroPlatformSettings;
@@ -74,7 +75,7 @@ public class ImportFileBasedIndex extends FileBasedIndexExtension<String, Void> 
     @NotNull
     @Override
     public DataExternalizer<Void> getValueExternalizer() {
-        return ScalarIndexExtension.VOID_DATA_EXTERNALIZER;
+        return VoidDataExternalizer.INSTANCE;
     }
 
     @NotNull
