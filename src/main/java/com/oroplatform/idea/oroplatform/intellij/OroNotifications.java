1eff76873d5349300fd1779f4f2c08a6daff4d5e
Aleksander Mosingiewicz <amosingiewicz@oroinc.com>
2023-12-19 15:37:52 +0100

    OPP-75: Deprecation removal.

--- src/main/java/com/oroplatform/idea/oroplatform/intellij/OroNotifications.java
+++ src/main/java/com/oroplatform/idea/oroplatform/intellij/OroNotifications.java
@@ -1,43 +1,50 @@
 package com.oroplatform.idea.oroplatform.intellij;
 
-import com.intellij.notification.Notification;
-import com.intellij.notification.NotificationGroup;
-import com.intellij.notification.NotificationType;
-import com.intellij.notification.Notifications;
+import com.intellij.notification.*;
+import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
 import com.oroplatform.idea.oroplatform.OroPlatformBundle;
 import com.oroplatform.idea.oroplatform.settings.OroPlatformSettings;
 import org.jetbrains.annotations.NotNull;
 
 class OroNotifications {
-    private final static NotificationGroup GROUP = NotificationGroup.balloonGroup(OroPlatformBundle.message("notifications.group"));
+    private final static NotificationGroup GROUP = NotificationGroupManager
+            .getInstance()
+            .getNotificationGroup("Oro Notifications");
 
     static void showPluginEnableNotification(@NotNull final Project project) {
         final Notification notification = GROUP.createNotification(
-            OroPlatformBundle.message("notifications.enablePluginTitle"),
-            OroPlatformBundle.message("notifications.enablePlugin"),
-            NotificationType.INFORMATION,
-            (thisNotification, event) -> {
-                thisNotification.expire();
+                OroPlatformBundle.message("notifications.enablePluginTitle"),
+                OroPlatformBundle.message("notifications.enablePlugin"),
+                NotificationType.INFORMATION);
 
-                final OroPlatformSettings settings = OroPlatformSettings.getInstance(project);
+        final OroPlatformSettings settings = OroPlatformSettings.getInstance(project);
+        //TODO add translations
+        notification.addAction(new NotificationAction(OroPlatformBundle.message("notifications.enablePlugin.accept")) {
+            @Override
+            public void actionPerformed(@NotNull AnActionEvent e, @NotNull Notification notification) {
+                notification.expire();
+                settings.setPluginEnabled(true);
+                showPluginEnabledNotification(project);
+            }
+        });
 
-                if("enable".equals(event.getDescription())) {
-                    settings.setPluginEnabled(true);
-                    showPluginEnabledNotification(project);
-                } else if("dismiss".equals(event.getDescription())) {
-                    settings.setPluginEnableDismissed(true);
-                }
-            });
-
+        notification.addAction(new NotificationAction(
+                OroPlatformBundle.message("notifications.enablePlugin.dismiss")) {
+            @Override
+            public void actionPerformed(@NotNull AnActionEvent e, @NotNull Notification notification) {
+                notification.expire();
+                settings.setPluginEnableDismissed(true);
+            }
+        });
         Notifications.Bus.notify(notification, project);
     }
 
     private static void showPluginEnabledNotification(@NotNull final Project project) {
         Notifications.Bus.notify(GROUP.createNotification(
-            OroPlatformBundle.message("notifications.group"),
-            OroPlatformBundle.message("notifications.pluginEnabled"),
-            NotificationType.INFORMATION, null
+                OroPlatformBundle.message("notifications.group"),
+                OroPlatformBundle.message("notifications.pluginEnabled"),
+                NotificationType.INFORMATION
         ), project);
     }
 }
